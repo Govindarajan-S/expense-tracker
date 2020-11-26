e0c51c1682522ff1d7204e1bb1cec1afaeeb67c9
Govindarjan Sivaraj <sgovind2112@gmail.com>
2020-11-26 13:18:16 +0530

    Implement User and Expense Entity. Closes #1

---
+++ src/main/java/com/example/expensetracker/model/Expense.java
@@ -0,0 +1,59 @@
+package com.example.expensetracker.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Expense {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long expenseId;
+	
+	private Double expenseAmount;
+	
+	private String description;
+	
+	private String notes;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Long getExpenseId() {
+		return expenseId;
+	}
+
+	public void setExpenseId(Long expenseId) {
+		this.expenseId = expenseId;
+	}
+
+	public Double getExpenseAmount() {
+		return expenseAmount;
+	}
+
+	public void setExpenseAmount(Double expenseAmount) {
+		this.expenseAmount = expenseAmount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+}
---
+++ src/main/java/com/example/expensetracker/model/User.java
@@ -0,0 +1,72 @@
+package com.example.expensetracker.model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private LocalDate dateOfBirth;
+	
+	private String mailId;
+	
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+	private Set<Expense> expenses = new HashSet<>();
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getMailId() {
+		return mailId;
+	}
+
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	
+}
