7b54701c5b1884a573c7a1f221228259aad32985
Govindarjan Sivaraj <sgovind2112@gmail.com>
2020-11-28 21:25:05 +0530

    Implement delete and update expenses. Closes #6

--- src/main/java/com/example/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/example/expensetracker/controller/ExpenseController.java
@@ -2,10 +2,12 @@ package com.example.expensetracker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.expensetracker.model.Expense;
@@ -14,13 +16,13 @@ import com.example.expensetracker.repository.ExpenseRepository;
 import com.example.expensetracker.repository.UserRepository;
 
 @Controller
-public class ExpenseContoller {
+public class ExpenseController {
 
 	private final ExpenseRepository expenseRepository;
 
 	private final UserRepository userRepository;
 
-	public ExpenseContoller(ExpenseRepository expenseRepository, UserRepository userRepository) {
+	public ExpenseController(ExpenseRepository expenseRepository, UserRepository userRepository) {
 		this.expenseRepository = expenseRepository;
 		this.userRepository = userRepository;
 	}
@@ -48,6 +50,30 @@ public class ExpenseContoller {
 		expenseRepository.save(expense);
 		user.getExpenses().add(expense);
 		userRepository.save(user);
+		return "redirect:/users/{id}/expense";
+	}
+
+	@GetMapping
+	@RequestMapping({ "/users/{id}/expense/{expenseId}/edit" })
+	public String getExpenseUpdatePage(@PathVariable Long id, @PathVariable Long expenseId, Model model) {
+		model.addAttribute("userId", id);
+		model.addAttribute("expense", expenseRepository.findById(expenseId).get());
+		return "updateExpense";
+	}
+
+	@PutMapping
+	@RequestMapping({ "/users/{id}/expense/{expenseId}/save" })
+	public String updateExpense(@PathVariable Long id, @PathVariable Long expenseId, Expense expense) {
+		expenseRepository.save(expense);
+		return "redirect:/users/{id}/expense";
+	}
+
+	@DeleteMapping
+	@RequestMapping({ "/users/{id}/expense/{expenseId}/delete" })
+	public String doDeleteExpense(@PathVariable Long id, @PathVariable Long expenseId) {
+		User user = userRepository.findById(id).get();
+		user.getExpenses().remove(expenseRepository.findById(expenseId).get());
+		expenseRepository.deleteById(expenseId);
 		return "redirect:/users/{id}/expense";
 	}
 }
